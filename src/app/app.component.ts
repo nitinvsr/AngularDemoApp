18d63d23359821b2e1fadc96334d41175211a734
nitinvsr <nitinsaipokuri@gmail.com>
2020-09-21 10:14:20 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'yt-ang';
+  title = 'Project CC';
+  date = new Date();
+  name = 'nitin';
 }
